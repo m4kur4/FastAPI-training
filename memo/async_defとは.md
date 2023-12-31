149dd884ae3dfa1f909bdd174044b3f17719ca18
makura016 <i.shiiba.016i@gmail.com>
2023-12-31 16:24:47 +0900

    update router

---
+++ memo/async_defとは.md
@@ -0,0 +1,7 @@
+## async defとは
+* 参考
+ - https://qiita.com/ffggss/items/e4c06f86fb28a62948e0
+  - この記事では、基本的に`def`を使うべきとされている
+  - 非同期は必要な時だけ使うスタンスで良さそう
+
+  
--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 
+from api.routers import task, done
+
 app = FastAPI()
 
+# ▽Hello world
+# @app.get("/hello")
+# def hello():
+#     return {"message": "hello world!"}
 
-@app.get("/hello")
-async def hello():
-    return {"message": "hello world!"}
+# ルーターからパスオペレーション関数（？）をインクルード
+app.include_router(task.router)
+app.include_router(done.router)
