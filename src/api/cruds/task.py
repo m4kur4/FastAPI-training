5b423143fd8a19d17e77d135341ed04efd07056f
makura016 <i.shiiba.016i@gmail.com>
2024-01-01 10:53:08 +0900

    add read method

--- src/api/cruds/task.py
+++ src/api/cruds/task.py
@@ -11,3 +11,19 @@ async def create_task(
     await db.commit()
     await db.refresh(task) # DB上のデータを元にTaskインスタンスを更新する
     return task
+
+from typing import List, Tuple
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+
+async def get_tasks_with_done(db: AsyncSession) -> List[Tuple[int, str, bool]]:
+    result: Result = await (
+        db.execute(
+            select(
+                task_model.Task.id,
+                task_model.Task.title,
+                task_model.Done.id.isnot(None).label("done")
+            ).outerjoin(task_model.Done)
+        )
+    )
+    return result.all()
--- src/api/routers/task.py
+++ src/api/routers/task.py
@@ -9,8 +9,8 @@ import api.schemas.task as task_schema
 router = APIRouter()
 
 @router.get("/tasks", response_model=List[task_schema.Task])
-async def list_tasks():
-    return [task_schema.Task(id="hogehoges", title="1つ目のTODOタスク")]
+async def list_tasks(db: AsyncSession = Depends(get_db)):
+    return await task_crud.get_tasks_with_done(db)
 
 @router.post("/tasks", response_model=task_schema.TaskCreateResponse)
 async def create_task(
